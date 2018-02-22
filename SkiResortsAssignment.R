e3b9da610439f0198d4fe98006c3a49d2fe5f6ef
Clayton Novotney <35281580+cnovotn-1631497@users.noreply.github.com>
2018-02-22 14:43:46 -0800

    Create SkiResortsAssignment.R

---
+++ SkiResortsAssignment.R
@@ -0,0 +1,21 @@
+# Base Code for the project
+# Install all important packages
+# Gather information from API at https://developer.worldweatheronline.com/api/ski-weather-api.aspx
+# Produce a data.table with all information discussed in proposal (•	Date for which the weather is forecasted
+•	Top, middle and bottom elevation weather forecasts
+•	Day max and min temperature in °C(Celsius) and °F(Fahrenheit)
+•	Location specific sunrise and sunset, moonrise and moonset in the local time.
+•	Hourly temperature in °C(Celsius)
+•	Freezing Level in (meter)
+•	Expected Snowfall in (cm)
+•	Chance of Snow in (%)
+•	Wind Speed in mph (miles per hour) and kmph (Kilometer per hour)
+•	16-Point compass wind direction
+•	A unique weather condition code
+•	Weather description text
+•	Weather symbol image URL
+•	Precipitation in millimeter (mm)
+•	Humidity in percentage (%)
+•	Visibility in kilometer (km)
+•	Pressure in millibar (mb)
+•	Cloud cover in percentage (%)
